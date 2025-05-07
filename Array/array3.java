2d6b55b61e2cc1f307599fb12b008a9c62cc1728
gilbaay <ragatra00@gmail.com>
2025-05-07 19:31:25 +0700

    Add files via upload

---
+++ Array/array3.java
@@ -0,0 +1,18 @@
+// Nama file: array3.java
+
+public class array3 {
+    public static void main(String args[]) {
+        int a[] = {10, 30, 50, 70, 90};  // Mendeklarasikan array dengan nilai langsung
+        int total = 0;  // Variabel untuk menyimpan total jumlah
+
+        System.out.println("Index\tNilai");
+
+        // Loop untuk menampilkan index dan nilai array, serta menghitung total jumlah
+        for (int i = 0; i < a.length; i++) {
+            total += a[i];  // Menambahkan nilai array ke total
+            System.out.println(i + "\t" + a[i]);  // Menampilkan index dan nilai array
+        }
+
+        System.out.println("Total jumlah isi array: " + total);  // Menampilkan total jumlah
+    }
+}
